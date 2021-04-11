c89fe3fa8d6dfaffb30522e85c044b34800a16fa
mackysoft <mackysoft0129@gmail.com>
2021-04-11 18:34:16 +0900

    Update UnityPackageExporter.cs

--- Assets/ExportPackageExample/Editor/UnityPackageExporter.cs
+++ Assets/ExportPackageExample/Editor/UnityPackageExporter.cs
@@ -4,37 +4,33 @@ using UnityEditor;
 namespace ExportPackageExample.Editor {
 	public static class UnityPackageExporter {
 
-		const string k_PackagePath = "ExportPackageExample";
+		// The name of the unitypackage to output.
 		const string k_PackageName = "ExportPackageExample";
-		const string k_TargetPath = "Build";
+
+		// The path to the package under the `Assets/` folder.
+		const string k_PackagePath = "ExportPackageExample";
+
+		// Path to export to.
+		const string k_ExportPath = "Build";
 
 		public static void Export () {
-			ExportPackage($"{k_TargetPath}/{k_PackageName}.unitypackage");
-			ExportVersion($"{k_TargetPath}/version.txt");
+			ExportPackage($"{k_ExportPath}/{k_PackageName}.unitypackage");
 		}
 
 		public static string ExportPackage (string exportPath) {
+			// Ensure export path.
 			var dir = new FileInfo(exportPath).Directory;
 			if (dir != null && !dir.Exists) {
 				dir.Create();
 			}
 
+			// Export
 			AssetDatabase.ExportPackage(
 				$"Assets/{k_PackagePath}",
 				exportPath,
 				ExportPackageOptions.Recurse
 			);
 
-			return Path.GetFullPath(exportPath);
-		}
-
-		public static string ExportVersion (string exportPath) {
-			var dir = new FileInfo(exportPath).Directory;
-			if (dir != null && !dir.Exists) {
-				dir.Create();
-			}
-
-			File.WriteAllText(exportPath,k_PackageName);
 			return Path.GetFullPath(exportPath);
 		}
 
